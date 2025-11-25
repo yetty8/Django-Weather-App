164c92e73b935c9314a3de07e72925c8c1e32c13
yetbarek temesgen <yetbarek1@gmail.com>
2025-11-25 17:03:49 -0500

    Remove venv

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,3 +73,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'    # folder for collectstatic (Render ser
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
